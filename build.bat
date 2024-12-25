6e7c5f9b00d2d7cd2c4b0d1253a6e82ea9ca5b57
nekkochan <nekkochan@gmail.com>
2024-12-25 11:00:17 +0700

    Fix build script

--- build.bat
+++ build.bat
@@ -8,4 +8,9 @@ cd build
 cmake -G "MinGW Makefiles" ..
 make
 
+copy /Y deps\SDL2\SDL2.dll .
+copy /Y mingw\mingw64\bin\libgcc_s_seh-1.dll .
+copy /Y "mingw\mingw64\bin\libstdc++-6.dll" .
+copy /Y mingw\mingw64\bin\libwinpthread-1.dll .
+
 cd %CURRENT_DIRECTORY%
